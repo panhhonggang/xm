6d21c632eec3e4e6e6ca64eb61f5588926d37886
chenchangping <chenchangping@foxmail.com>
2017-10-19 10:45:43 +0800

    2017-10-19 更新数据库字典 添加数据库文件

---
+++ doc/架构设计/开发文档/pubdata.sql
@@ -0,0 +1,205 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 本地数据库
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : pubdata
+
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
+
+Date: 2017-10-19 10:43:55
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for pub_auth_group
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_auth_group`;
+CREATE TABLE `pub_auth_group` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `title` varchar(20) NOT NULL COMMENT '分组名称',
+  `status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '分组状态：默认“1”为开启',
+  `rules` varchar(20) NOT NULL COMMENT '分组的权限',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_auth_group_access
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_auth_group_access`;
+CREATE TABLE `pub_auth_group_access` (
+  `uid` int(11) unsigned NOT NULL COMMENT '关联的供销商ID号',
+  `group_id` int(11) unsigned NOT NULL COMMENT '关联的用户组ID号'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_auth_rule
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_auth_rule`;
+CREATE TABLE `pub_auth_rule` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `pid` int(11) unsigned NOT NULL COMMENT '规则的父级ID',
+  `name` varchar(80) NOT NULL COMMENT '规则路径 “模块/控制器/方法/”',
+  `title` varchar(20) NOT NULL COMMENT '规则的自定义名称',
+  `status` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '规则状态：“0”为禁用，“1”为启用(默认)',
+  `condition` varchar(50) NOT NULL COMMENT '规则的条件',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_devices
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_devices`;
+CREATE TABLE `pub_devices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `device_code` int(20) unsigned NOT NULL COMMENT '设备编码',
+  `device_statu` tinyint(1) unsigned NOT NULL COMMENT '设备状态：1已入库，2待激活，3已激活',
+  `created_at` datetime NOT NULL COMMENT '添加时间',
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`,`device_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_devices_statu
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_devices_statu`;
+CREATE TABLE `pub_devices_statu` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `device_id` int(11) unsigned NOT NULL COMMENT '关联的设备ID号',
+  `device_statu` tinyint(1) unsigned NOT NULL COMMENT '设备状态',
+  `updated_at` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`,`device_statu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_filters
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_filters`;
+CREATE TABLE `pub_filters` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `device_id` int(11) unsigned NOT NULL COMMENT '关联的设备ID号',
+  `filter1` int(3) unsigned NOT NULL COMMENT '滤芯1',
+  `filter2` int(3) unsigned NOT NULL COMMENT '滤芯2',
+  `filter3` int(3) unsigned NOT NULL COMMENT '滤芯3',
+  `filter4` int(3) unsigned DEFAULT NULL COMMENT '滤芯4',
+  `filter5` int(3) unsigned DEFAULT NULL COMMENT '滤芯5',
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_hire
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_hire`;
+CREATE TABLE `pub_hire` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `hire_id` int(11) unsigned NOT NULL COMMENT '租赁编号',
+  `device_id` int(11) unsigned NOT NULL COMMENT '关联的设备ID号',
+  `user_id` int(11) unsigned NOT NULL COMMENT '关联的用户ID号',
+  `lease_way` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '租赁方式：“1”为计时，“2”为计量',
+  `rental_fee` decimal(15,2) unsigned NOT NULL COMMENT '租赁费用',
+  `begin_time` datetime NOT NULL COMMENT '租赁开始时间',
+  `end_time` datetime NOT NULL COMMENT '租赁结束时间',
+  PRIMARY KEY (`id`),
+  KEY `hire_id` (`hire_id`,`device_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_leaevl
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_leaevl`;
+CREATE TABLE `pub_leaevl` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `vendors_id` int(11) unsigned NOT NULL COMMENT '关联的供销商ID号',
+  `parent_vid` int(11) unsigned NOT NULL COMMENT '供销商的父级ID',
+  `path` varchar(11) NOT NULL COMMENT '供销商的层级关系',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_loglist
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_loglist`;
+CREATE TABLE `pub_loglist` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `user_id` int(11) unsigned NOT NULL COMMENT '关联的用户ID号',
+  `content` varchar(255) NOT NULL COMMENT '操作内容',
+  `time` datetime NOT NULL COMMENT '操作时间',
+  `ip` varchar(15) NOT NULL COMMENT '操作IP',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_orders
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_orders`;
+CREATE TABLE `pub_orders` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `order_id` int(11) unsigned NOT NULL COMMENT '订单编号',
+  `device_id` int(11) unsigned NOT NULL COMMENT '关联的设备ID号',
+  `user_id` int(11) unsigned NOT NULL COMMENT '关联的用户ID号',
+  `goods_num` int(11) unsigned NOT NULL COMMENT '商品的购买数量',
+  `goods_price` decimal(15,2) unsigned NOT NULL COMMENT '商品的购买单价',
+  `goods_total` decimal(15,2) unsigned NOT NULL COMMENT '商品总金额',
+  `created_at` datetime NOT NULL COMMENT '订单创建时间',
+  `updated_at` datetime NOT NULL COMMENT '订单修改时间',
+  PRIMARY KEY (`id`),
+  KEY `order_id` (`order_id`,`device_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_users
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_users`;
+CREATE TABLE `pub_users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `name` varchar(15) NOT NULL COMMENT '用户名字',
+  `phone` varchar(11) NOT NULL COMMENT '手机号码',
+  `device_id` int(11) unsigned NOT NULL COMMENT '关联的设备ID号',
+  `user_status` tinyint(1) unsigned NOT NULL COMMENT '用户状态',
+  `login_time` datetime DEFAULT NULL COMMENT '最后登陆的时间',
+  `login_ip` varchar(15) DEFAULT NULL COMMENT '最后登陆的IP地址',
+  `created_at` datetime NOT NULL COMMENT '添加时间',
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`,`phone`,`device_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_vendors
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_vendors`;
+CREATE TABLE `pub_vendors` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `user` varchar(32) NOT NULL COMMENT '供销商登陆的用户名',
+  `name` varchar(32) NOT NULL COMMENT '供销商的名字',
+  `password` varchar(32) NOT NULL COMMENT '供销商登陆的密码',
+  `phone` varchar(11) NOT NULL COMMENT '供销商手机号码',
+  `email` varchar(50) DEFAULT NULL COMMENT '供销商邮箱',
+  `address` varchar(255) DEFAULT NULL COMMENT '供销商地址',
+  `leavel` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '供销商级别：默认为“1”级',
+  PRIMARY KEY (`id`),
+  KEY `user` (`user`,`name`,`password`,`email`,`phone`,`leavel`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for pub_wechat
+-- ----------------------------
+DROP TABLE IF EXISTS `pub_wechat`;
+CREATE TABLE `pub_wechat` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `user_id` int(11) unsigned NOT NULL COMMENT '关联的用户ID号',
+  `open_id` varchar(50) NOT NULL COMMENT '微信的ID号',
+  `nickname` varchar(50) NOT NULL COMMENT '微信昵称',
+  `head` varchar(255) DEFAULT NULL COMMENT '头像',
+  `sex` tinyint(1) unsigned DEFAULT NULL COMMENT '性别',
+  `area` varchar(255) DEFAULT NULL COMMENT '地区',
+  `address` varchar(255) DEFAULT NULL COMMENT '地址',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`,`open_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
